c9515d97e40d527d21930d20509000717a4d8a55
Dustin Brewer <mannkind@thenullpointer.net>
2021-11-02 02:44:42 +0000

    Fix test

--- LitterRobotTest/Liasons/MQTTLiasonTest.cs
+++ LitterRobotTest/Liasons/MQTTLiasonTest.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using TwoMQTT.Utils;
+using TwoMQTT.Interfaces;
 
 namespace LitterRobotTest.Liasons;
 
